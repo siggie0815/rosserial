70bf313efefb22b3d2bbdec7f0fee4daac2ba744
Markus Sigg <dev.siggie@gmail.com>
2016-12-04 01:29:37 +0100

    [rosserial_client] Apply ROS Code Style.

--- rosserial_client/src/ros_lib/time.cpp
+++ rosserial_client/src/ros_lib/time.cpp
@@ -36,57 +36,71 @@
 
 namespace ros
 {
-  void normalizeSecNSec(uint32_t& sec, uint32_t& nsec){
-    uint32_t nsec_part= nsec % 1000000000UL;
-    uint32_t sec_part = nsec / 1000000000UL;
-    sec += sec_part;
-    nsec = nsec_part;
-  }
+void normalizeSecNSec(uint32_t &sec, uint32_t &nsec)
+{
+  uint32_t nsec_part = nsec % 1000000000UL;
+  uint32_t sec_part = nsec / 1000000000UL;
+  sec += sec_part;
+  nsec = nsec_part;
+}
 
-  Time& Time::fromNSec(int32_t t)
-  {
-    sec = t / 1000000000;
-    nsec = t % 1000000000;
-    normalizeSecNSec(sec, nsec);
-    return *this;
-  }
+Time &Time::fromNSec(int32_t t)
+{
+  sec = t / 1000000000;
+  nsec = t % 1000000000;
+  normalizeSecNSec(sec, nsec);
+  return *this;
+}
 
-  Time& Time::operator +=(const Duration &rhs)
-  {
-    sec += rhs.sec;
-    nsec += rhs.nsec;
-    normalizeSecNSec(sec, nsec);
-    return *this;
-  }
+Time &Time::operator+=(const Duration &rhs)
+{
+  sec += rhs.sec;
+  nsec += rhs.nsec;
+  normalizeSecNSec(sec, nsec);
+  return *this;
+}
 
-  Time& Time::operator -=(const Duration &rhs){
-    sec += -rhs.sec;
-    nsec += -rhs.nsec;
-    normalizeSecNSec(sec, nsec);
-    return *this;
-  }
-  
-  bool Time::operator <(const Time &rhs){
-    if (sec < rhs.sec) return true;
-    if (sec == rhs.sec && nsec < rhs.nsec) return true;
-    return false;
-  }
-  
-  bool Time::operator >(const Time &rhs){
-    if (sec > rhs.sec) return true;
-    if (sec == rhs.sec && nsec > rhs.nsec) return true;
-    return false;
-  }
-  
-  bool Time::operator <=(const Time &rhs){
-    if (sec > rhs.sec) return false;
-    if (sec == rhs.sec && nsec > rhs.nsec) return false;
+Time &Time::operator-=(const Duration &rhs)
+{
+  sec += -rhs.sec;
+  nsec += -rhs.nsec;
+  normalizeSecNSec(sec, nsec);
+  return *this;
+}
+
+bool Time::operator<(const Time &rhs)
+{
+  if (sec < rhs.sec)
     return true;
-  }
-  
-  bool Time::operator >=(const Time &rhs){
-    if (sec < rhs.sec) return false;
-    if (sec == rhs.sec && nsec < rhs.nsec) return false;
+  if (sec == rhs.sec && nsec < rhs.nsec)
     return true;
-  }
+  return false;
+}
+
+bool Time::operator>(const Time &rhs)
+{
+  if (sec > rhs.sec)
+    return true;
+  if (sec == rhs.sec && nsec > rhs.nsec)
+    return true;
+  return false;
+}
+
+bool Time::operator<=(const Time &rhs)
+{
+  if (sec > rhs.sec)
+    return false;
+  if (sec == rhs.sec && nsec > rhs.nsec)
+    return false;
+  return true;
+}
+
+bool Time::operator>=(const Time &rhs)
+{
+  if (sec < rhs.sec)
+    return false;
+  if (sec == rhs.sec && nsec < rhs.nsec)
+    return false;
+  return true;
+}
 }
